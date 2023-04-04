3a9f8f548101fe0d39a1c4e3febf10fc19ff4a84
Saswankar Bura Gohain <111171338+Saswankar1@users.noreply.github.com>
2023-04-04 19:03:24 +0530

    Update global_var.sol

--- notes/global_var.sol
+++ notes/global_var.sol
@@ -1,1 +1,12 @@
 
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract glob_var{
+    
+   function demo() public view returns(uint block_no, uint timestamp, address msgSender){
+       return(block.number, block.timestamp, msg.sender);
+   }
+
+}
